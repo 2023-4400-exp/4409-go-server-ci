715625e027108e73304d737b2b046ce8d398cf2d
agowo <m20029@g.metro-cit.ac.jp>
2024-01-09 13:49:07 +0900

    message wo kousinnsita

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4409", "message": "helloogawadesu"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4409", "message": "ikiterudakedemarumouke"})
     }
 }
